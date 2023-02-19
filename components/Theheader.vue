b0543251cab650eb9404fdc91a92ea9b6da5c940
Sambath Vutha <sambathvutha168@gmail.com>
2023-02-19 23:35:57 +0700

    first commit

--- components/Theheader.vue
+++ components/Theheader.vue
@@ -1,42 +1,40 @@
 <template>
-    <div>
-        <!-- Navbar Start -->
-    <div class="container-fluid bg-light position-relative shadow">
-      <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0 px-lg-5">
-        <a href="" class="navbar-brand font-weight-bold text-secondary" style="font-size: 50px">
-          <i class="flaticon-043-teddy-bear"></i>
-          <span class="text-primary">KidKinder</span>
-        </a>
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div>
-            <NuxtLink to="/about" class=" text-lg">About page</NuxtLink>
+    <!----<div class=" min-h-full top-0 bg-slate-400 block ">
+        !-- Logo --
+        <img class=" w-24 h-24 ml-48 inline-block" src="~/assets/img/LDPC_logo1.png">
+    !-- Navbar Start --
+    <nav class="container mx-auto p-4 inline-block justify-between">
+        <ul class=" flex gap-7">
+            <li><nuxt-link to="/">Home</nuxt-link></li>
+            <li><nuxt-link to="/Test">Test</nuxt-link></li>
+            <li><nuxt-link to="/products">Products</nuxt-link></li>
+            <li><nuxt-link to="/about">About</nuxt-link></li>
+            <li><nuxt-link to="/login">Login</nuxt-link></li>
+            <li><nuxt-link to="/register">Register</nuxt-link></li>
+        </ul>
+    </nav>-->
+
+    <div class=" fixed top-0 w-full bg-slate-300 my-0 lg:justify-between">
+        <div class=" inline-block">
+            <img class=" w-24 h-24 ml-40 inline-block" src="~/assets/img/LDPC_logo1.png">
         </div>
-      </nav>
-    </div>
+        <nav class="inline-flex mx-auto p-4 justify-between ml-80">         
+            <ul class=" flex gap-7 font-bold">
+                <li><nuxt-link to="/">Home</nuxt-link></li>
+                <li><nuxt-link to="/Test">Test</nuxt-link></li>
+                <li><nuxt-link to="/products">Products</nuxt-link></li>
+                <li><nuxt-link to="/about">About</nuxt-link></li>
+                <li><nuxt-link to="/login">Login</nuxt-link></li>
+                <li><nuxt-link to="/register">Register</nuxt-link></li>
+            </ul>
+        </nav>
+        <div class="inline-block -space-x-2 ml-80">
+            <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white" src="~/assets/img/user.jpg" alt="" />
+        </div>  
+    
     <!-- Navbar End -->
     <!-- Header Start -->
-    <div class="container-fluid bg-primary px-0 px-md-5 mb-5">
-      <div class="row align-items-center px-3">
-        <div class="col-lg-6 text-center text-lg-left">
-          <h4 class="text-white mb-4 mt-5 mt-lg-0">Kids Learning Center</h4>
-          <h1 class="display-3 font-weight-bold text-white">
-            New Approach to Kids Education
-          </h1>
-          <p class="text-white mb-4">
-            Sea ipsum kasd eirmod kasd magna, est sea et diam ipsum est amet sed
-            sit. Ipsum dolor no justo dolor et, lorem ut dolor erat dolore sed
-            ipsum at ipsum nonumy amet. Clita lorem dolore sed stet et est justo
-            dolore.
-          </p>
-          <a href="" class="btn btn-secondary mt-1 py-3 px-5">Learn More</a>
-        </div>
-        <div class="col-lg-6 text-center text-lg-right">
-          <img class="img-fluid mt-5" src="~/assets/img/about-1.jpg" alt="no img">/>
-        </div>
-      </div>
-    </div>
+  
     <!-- Header End -->
     </div>
 </template>
